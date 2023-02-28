7b5e7029504053fa774542733193ef19a3542427
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-02-28 07:08:50 +0530

    [Sangmeshwar] Add: Binary Search Program Added

---
+++ DataStructureAlgoritmProblems/DataStructureAlgoritmProblems/BinarySearchWord.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgoritmProblems
+{
+    public class BinarySearchWord
+    {
+        bool check = false;
+        public string FILEPATH = @"C:\Users\hp\source\repos\DataStructureAlgoritmProblems\DataStructureAlgoritmProblems\Word.txt";
+        public void Search()
+        {
+            string words = File.ReadAllText(FILEPATH);
+            string[] wordsList = words.Split(",");
+            Console.Write("\nGive the Word To Search: ");
+            string searchWord = Console.ReadLine();
+            foreach (string word in wordsList)
+            {
+                if (word.Equals(searchWord))
+                {
+                    Console.WriteLine("\n\'" + searchWord + "\' is Found in Words List");
+                    check = true;
+                    break;
+                }
+            }
+            if (check == false)
+                Console.WriteLine("\n\'" + searchWord + "\' is Not Found in Words List");
+        }
+    }
+}
