b23871d9aa1743672b7a3c7b62ca0d2f87ced841
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-03-02 06:47:25 +0530

    [Sangmeshwar] Add: Insertion Sort Problem Added

---
+++ DataStructureAlgoritmProblems/DataStructureAlgoritmProblems/InsertionSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgoritmProblems
+{
+    public class Insertion_Sort
+    {
+        public void InsertionSort()
+        {
+            Console.WriteLine("---Welcome to Insertion Sorting---\n");
+            int[] arr = new int[5] { 8, 5, 7, 3, 1 };
+            int n = 5, i, j, val;
+            Console.WriteLine("Insertion Sort\n");
+            Console.Write("Initial array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            for (i = 1; i < n; i++)
+            {
+                val = arr[i];
+                for (j = i - 1; j >= 0;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.Write("\nSorted Array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
